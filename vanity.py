1a4167b66777a74f2cf8db66c166fd454b0bb692
Mohit Maurya <notyourshacker6@gmail.com>
2024-06-16 10:34:46 +0530

    Update vanity.py
    fixed

--- vanity.py
+++ vanity.py
@@ -4,12 +4,12 @@ import discord
 from discord.ext import commands
 client = commands.Bot(command_prefix='', self_bot=True)
 token = ""
-guild_id = ["1149228142314139648"]
+guild_id = 1149228142314139648
 @client.event
 async def on_guild_update(before, after):
     async for entry in after.audit_logs(
                 limit=1):
-      if entry.user.id == after.owner_id or entry.user.id == client.user.id or after.id not in guild_id:return 
+      if entry.user.id == after.owner_id or entry.user.id == client.user.id or after.id != guild_id:return 
       if before.vanity_url_code!= after.vanity_url_code: 
        try:await after.edit(vanity_code=before.vanity_url_code)        
        except Exception as e:print(e)
